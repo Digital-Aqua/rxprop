665ae9cbebe9e6c9316c365ebefa31a4fa776544
Aaron Waters <aaron@aaronwaters.net>
2025-06-11 12:15:19 +1000

    Added a few extra exports

--- Source/rxprop/__init__.py
+++ Source/rxprop/__init__.py
@@ -4,17 +4,21 @@ Super-simple reactive properties.
 __version__ = "0.1.0"
 
 from .computed_property import computed
+from .lifetime import Lifetime
 from .notifier import ChangeNotifierBase, Notifier, PChangeNotifier
 from .reactive import watchf
 from .reactive_list import ReactiveList
+from .typed_property import TypedProperty
 from .value_property import value
 from .watch_property import watchp
 
 __all__ = [
     "computed",
+    "Lifetime",
     "ChangeNotifierBase", "Notifier", "PChangeNotifier",
     "watchf",
     "ReactiveList",
+    "TypedProperty",
     "value",
     "watchp",
 ]
